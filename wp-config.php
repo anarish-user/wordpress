7b967a771fdda630420db5edd0554e1be7eece24
anarish-user <charu.maheshwari@anarish.com>
2025-05-05 16:02:27 +0530

    heroku deployment working

--- wp-config.php
+++ wp-config.php
@@ -2,53 +2,31 @@
 /**
  * The base configuration for WordPress
  *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * Localized language
- * * ABSPATH
- *
  * @link https://wordpress.org/support/article/editing-wp-config-php/
  *
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+// ** Database settings ** //
+if ( getenv('JAWSDB_URL') ) {
+    $url = parse_url(getenv('JAWSDB_URL'));
 
-/** Database username */
-define( 'DB_USER', 'root' );
+    define( 'DB_NAME',     ltrim($url['path'], '/') );
+    define( 'DB_USER',     $url['user'] );
+    define( 'DB_PASSWORD', $url['pass'] );
+    define( 'DB_HOST',     $url['host'] );
+} else {
+    // Local fallback
+    define( 'DB_NAME', 'local' );
+    define( 'DB_USER', 'root' );
+    define( 'DB_PASSWORD', 'root' );
+    define( 'DB_HOST', 'localhost' );
+}
 
-/** Database password */
-define( 'DB_PASSWORD', 'root' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
-
-/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+// ** Authentication unique keys and salts. Generate new ones from https://api.wordpress.org/secret-key/1.1/salt/ **
 define( 'AUTH_KEY',          'H?o$a0RCnJhuMkVr)S>WXBOCyQ@W{uEjhKa1DI;{k##i-dCR6DV1_N~!w~e@B?ia' );
 define( 'SECURE_AUTH_KEY',   '^:]4_cw?GB<9_6!`iWJ2iD8jxF/>mQu+.CFx6Q2oIF95d.qW0vA;~x6r &-FOn-d' );
 define( 'LOGGED_IN_KEY',     '(|?5FoA 3OJ-VxZ+fHV-T N?NPPCa7ei2Rm9ewk<mb55V3n~&4GDVCl}>KmnMqeg' );
@@ -59,40 +37,24 @@ define( 'LOGGED_IN_SALT',    'tnUJ?S,Pd#!T/%9M3o#3)q}y1C%/3fY@(P OPnl-.80Yefj.W-
 define( 'NONCE_SALT',        '5h)dR_4MT<#03$Uc}p@d.T@nXBbyq3y,V%M-Inulw|gD@;Lw)=F?L_Fu9T3u/C@o' );
 define( 'WP_CACHE_KEY_SALT', '+m&WX4q!$q5~aaOT6~YeDe!DBzw,BlT(}12Hzan$?Rh$IB0ZY8f^eJ!HDg.xx1xb' );
 
-
-/**#@-*/
-
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
+// ** Table prefix **
 $table_prefix = 'wp_';
 
+// ** Environment-specific settings **
+define( 'WP_ENVIRONMENT_TYPE', getenv('JAWSDB_URL') ? 'production' : 'local' );
+define( 'WP_DEBUG', !getenv('JAWSDB_URL') );
 
-/* Add any custom values between this line and the "stop editing" line. */
-
-
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
-if ( ! defined( 'WP_DEBUG' ) ) {
-	define( 'WP_DEBUG', false );
+if (!getenv('JAWSDB_URL')) {
+    define( 'WP_DEBUG_LOG', true );
+    define( 'WP_DEBUG_DISPLAY', true );
+} else {
+    define( 'DISALLOW_FILE_EDIT', true );
+    define( 'DISALLOW_FILE_MODS', true );
 }
 
-define( 'WP_ENVIRONMENT_TYPE', 'local' );
-/* That's all, stop editing! Happy publishing. */
+// ** Site URLs - Optional but useful for environments **
+define( 'WP_HOME', getenv('WP_HOME') ?: 'http://localhost' );
+define( 'WP_SITEURL', getenv('WP_SITEURL') ?: 'http://localhost' );
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
